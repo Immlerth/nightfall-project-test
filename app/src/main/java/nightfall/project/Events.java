c3413dcb29f8133573fb3f8e1262df8139b98361
ajozefczak <vojak.vold@op.pl>
2021-01-26 13:33:02 +0100

    Dystansowa prayge

--- app/src/main/java/nightfall/project/Events.java
+++ app/src/main/java/nightfall/project/Events.java
@@ -98,17 +98,88 @@ public class Events {
         }
         else if(x == 59 && y == 52){
             if(czydrakkar == false){
+                String nazwapliku = Integer.toString(x) + Integer.toString(y) + ".txt";
+                BufferedReader czyt = null;
+                File file = new File("eventy/" +nazwapliku);
+                String sciezka = file.getAbsolutePath();
 
+                try
+                {
+                    czyt = new BufferedReader(new FileReader(sciezka));
+                    String line = "";
+                    while ((line = czyt.readLine()) != null) {
+                        String [] plik1 = line.split(";");
+                        id = plik1[0];
+                        opis = plik1[1];
+                    }
+                }
+                catch (FileNotFoundException e) {
+                    System.out.println("Blad" + e);
+                }
+                catch (IOException e) {
+                    System.out.println("Blad" + e);
+                }
+
+                System.out.println(opis);
+                czydrakkar = true;
             }
         }
-        else if(x == 59 && y == 52){
-            if(czydrakkar == true && czywioska == false){
 
-            }
-        }
         else if(x == 5 && y == 81){
             if(czystudnia == false){
+                String nazwapliku = Integer.toString(x) + Integer.toString(y) + ".txt";
+                BufferedReader czyt = null;
+                File file = new File("eventy/" +nazwapliku);
+                String sciezka = file.getAbsolutePath();
 
+                try
+                {
+                    czyt = new BufferedReader(new FileReader(sciezka));
+                    String line = "";
+                    while ((line = czyt.readLine()) != null) {
+                        String [] plik1 = line.split(";");
+                        id = plik1[0];
+                        opis = plik1[1];
+                    }
+                }
+                catch (FileNotFoundException e) {
+                    System.out.println("Blad" + e);
+                }
+                catch (IOException e) {
+                    System.out.println("Blad" + e);
+                }
+
+                System.out.println(opis);
+                czystudnia = true;
+            }
+        }
+
+        else if(x == 54 && y == 50){
+            if(czystudnia == true && czywioska == false){
+                String nazwapliku = Integer.toString(x) + Integer.toString(y) + ".txt";
+                BufferedReader czyt = null;
+                File file = new File("eventy/" +nazwapliku);
+                String sciezka = file.getAbsolutePath();
+
+                try
+                {
+                    czyt = new BufferedReader(new FileReader(sciezka));
+                    String line = "";
+                    while ((line = czyt.readLine()) != null) {
+                        String [] plik1 = line.split(";");
+                        id = plik1[0];
+                        opis = plik1[1];
+                    }
+                }
+                catch (FileNotFoundException e) {
+                    System.out.println("Blad" + e);
+                }
+                catch (IOException e) {
+                    System.out.println("Blad" + e);
+                }
+
+                System.out.println(opis);
+                czywioska = true;
             }
         }
         else if(x == 99 && y == 96){
